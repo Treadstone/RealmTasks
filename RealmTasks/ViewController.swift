194ba63141e7f62923a63a2ebbf4040c22fda248
AgtLucas <lucas@agtlucas.com>
2016-10-03 05:06:27 -0300

    Update ViewController

--- RealmTasks/ViewController.swift
+++ RealmTasks/ViewController.swift
@@ -7,6 +7,24 @@
 //
 
 import UIKit
+import RealmSwift
+
+// MARK: Model
+
+final class TaskList: Object {
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
 
 class ViewController: UITableViewController {
 
